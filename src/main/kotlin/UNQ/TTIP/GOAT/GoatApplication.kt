73cba92f8eb85aab35b9f057a67ddaf55bc20037
matu_ <matu_diaz_95@hotmail.com>
2022-09-09 15:59:50 -0300

    Spring Boot application created

---
+++ src/main/kotlin/UNQ/TTIP/GOAT/GoatApplication.kt
@@ -0,0 +1,11 @@
+package UNQ.TTIP.GOAT
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GoatApplication
+
+fun main(args: Array<String>) {
+	runApplication<GoatApplication>(*args)
+}
---
+++ src/test/kotlin/UNQ/TTIP/GOAT/GoatApplicationTests.kt
@@ -0,0 +1,13 @@
+package UNQ.TTIP.GOAT
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GoatApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
